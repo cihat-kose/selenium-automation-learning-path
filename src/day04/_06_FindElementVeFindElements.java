6edb920ec8aaca1fcec429c8adc9c0a3dd15e337
Cihat Köse <cihat.kose@hotmail.com>
2025-03-21 20:52:50 +0100

    Added _06_FindElementVeFindElements

---
+++ src/day04/_06_FindElementVeFindElements.java
@@ -0,0 +1,28 @@
+package day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utility.MyFunction;
+
+import java.util.List;
+
+public class _06_FindElementVeFindElements {
+    public static void main(String[] args) {
+        // When you examine in the browser, # for ids and class searches. can be put.
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353");
+
+        // WebElement element1=driver.findElement(By.id("faultyLocator"));
+        // no such element: Element not found
+
+        List<WebElement> elementler = driver.findElements(By.id("faultyLocator"));
+        System.out.println("elementler.size() = " + elementler.size());
+        // findElements locator sends an empty list that does not give an error if it is incorrect
+
+        MyFunction.wait(3);
+        driver.quit();
+    }
+}
