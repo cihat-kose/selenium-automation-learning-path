94f551e0e9c27304fa88f0031c323566c37cfee1
Cihat Köse <cihat.kose@hotmail.com>
2025-03-31 15:46:29 +0200

    feat: Added random number generation methods to MyFunction utility class - Added `generateRandomNumber(int min, int max)` to generate a random integer between a specified range (min to max). - Added `generateRandomNumber(int max)` to generate a random integer from 0 to a specified upper limit (max). - These methods enhance the utility class by providing additional functionality for generating random values needed in various testing scenarios.

--- src/utility/MyFunction.java
+++ src/utility/MyFunction.java
@@ -1,12 +1,18 @@
 package utility;
 public class MyFunction {
     public static void wait(int sec) {
-
         try {
             Thread.sleep(sec * 1000L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public static int generateRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min)) + min;
+    }
+
+    public static int generateRandomNumber(int max) {
+        return (int) (Math.random() * max);
     }
 }
