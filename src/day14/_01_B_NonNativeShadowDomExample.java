776c4444c09196dad4487d51a1c81a1aaa16ca85
Cihat Köse <cihat.kose@hotmail.com>
2025-04-09 19:03:39 +0200

    Rename class to _01_B_NonNativeShadowDomExample and clarify test purpose

--- src/day14/_01_B_NonNativeShadowDomExample.java
+++ src/day14/_01_B_NonNativeShadowDomExample.java
@@ -6,17 +6,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import utility.BaseDriver;
 
-public class _01_B_ShadowRoot extends BaseDriver {
+/**
+ * This test interacts with a custom web component that resembles Shadow DOM,
+ * but does not use native shadowRoot. It's useful for demonstrating the
+ * difference between native and non-native Shadow DOM-like structures.
+ */
+
+public class _01_B_NonNativeShadowDomExample extends BaseDriver {
 
     @Test
-    public void test() {
-
+    public void interactWithCustomComponentWithoutShadowRootTest() {
         driver.get("https://itytest.ccngroup.com.tr/Era/LoginPage?ReturnUrl=%2FEra");
 
+        // Locate the custom input component
         WebElement shadowHost = driver.findElement(By.xpath("(//dxbl-input-editor)[1]"));
+
+        // Try to directly access its internal input field
         WebElement usernameField = shadowHost.findElement(By.xpath("(//dxbl-input-editor)[1]/input"));
 
         wait.until(ExpectedConditions.elementToBeClickable(usernameField));
+
+        // Enter dummy text
         usernameField.sendKeys("username");
 
         waitAndClose();
